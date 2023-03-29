1f3338eedd16bdeb785f91e68e40376358f675ea
Diego Urbina <diego.urbina14774@est.uca.edu.ni>
2023-03-28 21:44:18 -0600

    Primer commit

---
+++ ReportesEjemplo/FrmPrincp.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Data.SqlClient
+Imports Microsoft.Reporting.WinForms
+
+Public Class FrmPrincipal
+    Sub verReporte(ByVal t As DataTable, ByVal ds As String, nombreRpt As String)
+        Try
+            Dim rpt As New ReportDataSource(ds, t)
+            FrmVistaPrevia.ReportViewer1.LocalReport.DataSources.Clear()
+            FrmVistaPrevia.ReportViewer1.LocalReport.DataSources.Add(rpt)
+            FrmVistaPrevia.ReportViewer1.LocalReport.ReportPath = nombreRpt
+            FrmVistaPrevia.ReportViewer1.Refresh()
+            FrmVistaPrevia.Show()
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al mostrar reporte")
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim tsql As String = "Select * from jobs"
+            Dim conex As New SqlConnection(My.Settings.BDHRConnectionString)
+            Dim da As New SqlDataAdapter(tsql, conex)
+            Dim t As New DataTable
+            da.Fill(t)
+            verReporte(t, "dsRegistros", "reportes\rptCargo.rdlc")
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al cargar reporte")
+        End Try
+    End Sub
+End Class
